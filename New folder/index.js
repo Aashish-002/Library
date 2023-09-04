fbef191e366e9ad839110911721d2705818aa8e9
Aashish P <aashishcoc007@gmail.com>
2023-09-04 21:13:08 +0530

    test

--- New folder/index.js
+++ New folder/index.js
@@ -1,7 +1,7 @@
 var cursor=document.querySelector(".cursor")
 var a=document.querySelector(".bt")
 
-a.addEventListener('mousemove',(e)=>{
+document.addEventListener('mousemove',(e)=>{
     cursor.style.left=e.pageX +'px'
     cursor.style.top=e.pageY +'px'
 })
